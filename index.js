984eb7ab718a670fc792ccc9011a6100e63acbf4
lucas33400 <76783165+lucas33400@users.noreply.github.com>
2020-12-30 17:21:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true,
+        partials: ['MESSAGE', 'REACTION']
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+client.db = require('./db.json')
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+     
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    if (command.guildOnly && !message.guild) return message.channel.send('Cette commande ne peut être utilisée que dans un serveur.')
+    command.run(message, args, client)
+})
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member}`, new Discord.MessageEmbed()
+        .setDescription(`${member} Je te demande d'abord d'accepter le règlement pour pouvoir accéder aux autres chanel.
+
+        Le clan 1 est pour les joueurs qui tapes le **cauchemar** tout les jours et si tu souhaite progresser avec nous vers **l'ultra cauchemar**
+        
+        Le clan 2 est pour les joueurs qui tapes le **brutal **ou le **cauchemar**
+        
+        Et pour finir le clan 4 qui est réservé aux petits joueurs qui tapes le **facile, normal et difficile**
+        
+        Nous avons un projet pour un 4ème clan  mais sa reste entre toi et moi ! 🤫 
+        
+        Le discord est à ta disposition si tu as besoins d'aide notre équipe d'helpeurs et la ainsi que toute la communauté.
+        
+        Nous te souhaitons donc la **_BIENVENUE_**   Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+        .setColor('#00ff00'))
+    console.log(`${member}`, `${member.user.tag}`, 'est arrivé sur le serveur, nous sommes désormais', `${member.guild.memberCount}`)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel1).send(new Discord.MessageEmbed()
+        .setDescription(`${member.user.tag} a quitté le serveur... 😢`)
+        .setColor('#ff0000'))
+     console.log(`${member.user.tag}`, 'viens de quitter le serveur.')
+})
+ 
+client.on('messageReactionAdd', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.add(emoji.roles)
+    else reaction.users.remove(user)
+    console.log('viens d\'accepter le règlement.')
+})
+ 
+client.on('messageReactionRemove', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem || !reactionRoleElem.removable) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.remove(emoji.roles)
+    console.log('A tenté de supprimer son rôle.')
+})
+
+client.on('ready', () => {
+    const statuses = [
+        () => `${client.guilds.cache.size} serveurs`,
+        () => `${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} utilisateurs`
+    ]
+    let i = 0
+    setInterval(() => {
+        client.user.setActivity(statuses[i](), {type: 'PLAYING'})
+        i = ++i % statuses.length
+    }, 1e4)
+    
+})
+
+client.on('channelCreate', channel => {
+    if (!channel.guild) return
+    const muteRole = channel.guild.roles.cache.find(role => role.name === 'Muted')
+    if (!muteRole) return
+    channel.createOverwrite(muteRole, {
+        SEND_MESSAGES: false,
+        CONNECT: false,
+        ADD_REACTIONS: false
+    })
+})
